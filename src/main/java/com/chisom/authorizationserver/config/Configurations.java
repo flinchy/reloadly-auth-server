dcc88165afdef063ac4ca8c54292cb7d4f895b01
chisom <iwowochisom@gmail.com>
2021-02-28 14:13:43 +0100

    add configurations

---
+++ src/main/java/com/chisom/authorizationserver/config/Configurations.java
@@ -0,0 +1,45 @@
+package com.chisom.authorizationserver.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Chisom.Iwowo
+ */
+@Configuration
+public class Configurations {
+
+    /**
+     * add model mapper to spring ioc container.
+     *
+     * @return ModelMapper
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    /**
+     * password encoder.
+     *
+     * @return BCryptPasswordEncoder.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * @return RestTemplate.
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
