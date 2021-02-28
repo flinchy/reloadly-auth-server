fa7c487ba135224be477f2e248ed7a7354bdbcc8
chisom <iwowochisom@gmail.com>
2021-02-28 19:05:36 +0100

    enable scheduling

--- src/main/java/com/chisom/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/chisom/authorizationserver/AuthorizationServerApplication.java
@@ -2,8 +2,10 @@ package com.chisom.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
