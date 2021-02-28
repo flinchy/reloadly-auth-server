76818070cdd68f31f3ad5b0582544a4109ef65b3
chisom <iwowochisom@gmail.com>
2021-02-28 13:58:21 +0100

    add app constants

---
+++ src/main/java/com/chisom/authorizationserver/constant/AppConstant.java
@@ -0,0 +1,12 @@
+package com.chisom.authorizationserver.constant;
+
+/**
+ * @author Chisom.Iwowo
+ */
+public final class AppConstant {
+
+    private AppConstant() {
+    }
+
+    public static final String USER = "USER";
+}
