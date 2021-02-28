679a71e4a1c11dbf226d10e0e35b1e7e4824bf27
chisom <iwowochisom@gmail.com>
2021-02-28 14:14:12 +0100

    add app user repository

---
+++ src/main/java/com/chisom/authorizationserver/repository/AppUserRepository.java
@@ -0,0 +1,13 @@
+package com.chisom.authorizationserver.repository;
+
+import com.chisom.authorizationserver.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+}
